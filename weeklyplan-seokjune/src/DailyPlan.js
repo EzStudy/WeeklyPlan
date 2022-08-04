36eb18203eedc27a4463ab32fe52d77fca6f9b30
go-coding1 <rhtjrwnszyzy@gmail.com>
2022-08-04 20:45:51 +0900

    이번주 7요일 날짜 값 헤더에 출력
    이번주 7요일 날짜 값 헤더에 출력
    next : DatePicker 라이브러리 이용해서 선택한 날짜의 7요일을 동적으로 보여주기 현재는 useState안써서 동적 변화가 안될듯

--- weeklyplan-seokjune/src/DailyPlan.js
+++ weeklyplan-seokjune/src/DailyPlan.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./DailyPlan.css"
 
-function DailyPlan({day}){
+function DailyPlan({day, ymd}){
     return(
         <div className="DaliyPlan">
             <div className="DailyPlanHeader">
-                {day}
+                {day}요일, {ymd}
             </div>
             <div className="DailyPlanBody">
                 오늘의 일정입니다.
--- weeklyplan-seokjune/src/WeeklyPlanBody.js
+++ weeklyplan-seokjune/src/WeeklyPlanBody.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React , { useState } from "react";
 import DailyPlan from "./DailyPlan";
 
 import "./WeeklyPlanBody.css";
 
 function WeeklyPlanBody(){
+    const [days, setDays] = useState(['일','월','화','수','목','금','토']) 
+    const currentDay = new Date();
+    const theYear = currentDay.getFullYear();   //2022
+    const theMonth = currentDay.getMonth(); //8월 => 7, 6월 =>5
+    const theDate = currentDay.getDate();   //4일 => 4
+    const theDayofWeek = currentDay.getDay();   //일 : 0 월:1 화:2 수:3 목:4 금:5 토:6
+
+    const thisWeek = [];
+    for(let i=0; i<7; i++){
+        const resultDay = new Date(theYear, theMonth, theDate + (i- theDayofWeek));
+        const yyyy = resultDay.getFullYear();
+        let mm = Number(resultDay.getMonth()) + 1;
+        let dd = resultDay.getDate();
+
+        mm = String(mm).length === 1? '0'+mm : mm;
+        dd = String(dd).length === 1? '0'+dd : dd;
+
+        thisWeek[i] = yyyy+'-'+mm+'-'+dd;
+    }
+
     return(
         <div className="WeeklyPlanBody">
-            <DailyPlan day="일요일"/>
-            <DailyPlan day="월요일"/>
-            <DailyPlan day="화요일"/>
-            <DailyPlan day="수요일"/>
-            <DailyPlan day="목요일"/>
-            <DailyPlan day="금요일"/>
-            <DailyPlan day="토요일"/>
+            {thisWeek.map((week, index) =>(
+                <DailyPlan ymd={week} day={days[index]} />
+            ))};
         </div>
     )
 }
