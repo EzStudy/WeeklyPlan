d1f7a14272d4a678c793e402c4ac847d39f90baf
JeongYunha <yunhadev98@gmail.com>
2022-08-04 21:08:37 +0900

    feat: 주간 플래너 날짜 동적 할당

--- weeklyplan-yunha/src/component/DailyPlanBoxHeader.js
+++ weeklyplan-yunha/src/component/DailyPlanBoxHeader.js
@@ -1,12 +1,46 @@
 import React, {useState} from "react";
 import "../index.css";
+import DailyPlanBody from "./DailyPlanBody";
 
-export default function DailyPlanBoxHeader({day: d}) {
-    const [isDay, setDay] = useState(d);
+export default function DailyPlanBoxHeader() {
+// export default function DailyPlanBoxHeader({day: d}) {
+    // const [isDay, setDay] = useState(d);
 
     return(
-        <div>
-            <div className="daily-box-header">{isDay}</div>
+        <div className="bodyDisplay">
+            {
+                selectWeek().map( day => (
+                    <div>
+                        <div className="daily-box-header">{day}</div>
+                        <DailyPlanBody/>
+                    </div>
+                ))
+            }
+
         </div>
     )
+}
+
+function selectWeek() {
+    var currentDay = new Date();
+    var theYear = currentDay.getFullYear();
+    var theMonth = currentDay.getMonth();
+    var theDate  = currentDay.getDate();
+    var theDayOfWeek = currentDay.getDay();
+
+    var thisWeek = [];
+
+    for(var i=0; i<7; i++) {
+        var resultDay = new Date(theYear, theMonth, theDate + (i - theDayOfWeek));
+        var yyyy = resultDay.getFullYear();
+        var mm = Number(resultDay.getMonth()) + 1;
+        var dd = resultDay.getDate();
+
+        mm = String(mm).length === 1 ? '0' + mm : mm;
+        dd = String(dd).length === 1 ? '0' + dd : dd;
+
+        thisWeek[i] = yyyy + '-' + mm + '-' + dd;
+    }
+
+    return thisWeek;
 }
