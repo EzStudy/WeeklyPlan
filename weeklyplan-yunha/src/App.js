c1ba9fdc6d9f2e73fc077e6654e2609bc4f2e6ce
JeongYunha <yunhadev98@gmail.com>
2022-10-02 16:38:06 +0900

    feat: Firebase 연동

--- weeklyplan-yunha/src/App.js
+++ weeklyplan-yunha/src/App.js
@@ -1,9 +1,12 @@
 import SchedulerHeader from './component/SchedulerHeader';
-import React from "react";
 import SchedulerBody from "./component/SchedulerBody";
+import FirebaseTestComponent from "./component/FirebaseTestComponent";
+import {useState} from "react";
 
 function App() {
-
+    const [snap, setSnap] = useState(FirebaseTestComponent());
+    console.log('-------');
+    console.log(snap);
   return (
     <div className="App">
       <header className="flex justify-center items-center">
