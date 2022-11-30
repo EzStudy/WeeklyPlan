e01393c9937b6eafbab549f528cf53b744fb29e1
JeongYunha <yunhadev98@gmail.com>
2022-11-30 21:24:31 +0900

    feat: 일정 생성 모달 css 설정
    - color-picker 라이브러리 추가

--- weeklyplan-yunha/src/component/CreateScheduleBoxModal.js
+++ weeklyplan-yunha/src/component/CreateScheduleBoxModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorPicker from "./ColorPicker";
 
 export default function CreateScheduleBoxModal() {
     const [showModal, setShowModal] = React.useState(false);
@@ -7,14 +8,11 @@ export default function CreateScheduleBoxModal() {
             <button
                 className="bg-pink-500 text-white flex-auto w-40 mr-7"
                 type="button"
-                onClick={() => setShowModal(true)}
-            >
-                일정 생성
-            </button>
+                onClick={() => setShowModal(true)}>일정 생성</button>
             {showModal ? (
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative w-96 my-3">
+                        <div className="relative w-96 my-6">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
@@ -31,19 +29,24 @@ export default function CreateScheduleBoxModal() {
                     </span>
                                     </button>
                                 </div>
-                                {/*body*/}
-                                <div className="flex flex-col p-2">
-                                    <div className="flex flex-row">
-                                        <div className="mr-1.5">일정명</div>
-                                        <input type="text" name="title" placeholder="제목을 입력하세요"/>
-                                    </div>
-                                    <div className="flex flex-row">
-                                        <div>상세 일정</div>
-                                        <textarea></textarea>
-                                    </div>
-                                </div>
+                                {/*<div className="w-full">*/}
+                                    <table className="mt-2">
+                                        <tr>
+                                            <td className="pl-2.5 pt-1.5">일정명</td>
+                                            <td><input className="w-11/12 border-2" type="text" name="title" placeholder="제목을 입력하세요"/></td>
+                                        </tr>
+                                        <tr>
+                                            <td className="pl-2.5 pt-1.5">상세 일정</td>
+                                            <td><textarea className="w-11/12 h-36 items-end border-2" placeholder="상세 일정을 입력하세요"></textarea></td>
+                                        </tr>
+                                        <tr>
+                                            <td className="pl-2.5 pt-1.5 pb-1.5">일정 색상</td>
+                                            <td><ColorPicker /></td>
+                                        </tr>
+                                    </table>
+                                {/*</div>*/}
                                 {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
